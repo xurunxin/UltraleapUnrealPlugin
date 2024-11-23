6971d5774d6c9361bda7f29224a01c3489858c82
XuRunXin <xurx@live.com>
2024-11-24 01:18:57 +0800

    refactor(LeapVisualizer): Replace SetNiagaraVariableVec3 to SetVariableVec3 with FName

--- Source/UltraleapTrackingCore/Private/LeapVisualizer.cpp
+++ Source/UltraleapTrackingCore/Private/LeapVisualizer.cpp
@@ -6,7 +6,6 @@
  * between Ultraleap and you, your company or other organization.             *
  ******************************************************************************/
 
-
 #include "LeapVisualizer.h"
 #include "LeapUtility.h"
 #include "Kismet/GameplayStatics.h"
@@ -14,82 +13,74 @@
 #include "HeadMountedDisplayFunctionLibrary.h"
 
 // Sets default values
-ALeapVisualizer::ALeapVisualizer() 
-	: NSPlayerAreaBounds(nullptr)
-	, NSPTeleportRing(nullptr)
-	, Root(nullptr)
-	, PlayerAreaBoundsComponent(nullptr)
-	, TeleportRingComponent(nullptr)
-	, LeapPawn(nullptr)
-	, PlayerCameraManager(nullptr)
-	, World(nullptr)
+ALeapVisualizer::ALeapVisualizer()
+    : NSPlayerAreaBounds(nullptr), NSPTeleportRing(nullptr), Root(nullptr), PlayerAreaBoundsComponent(nullptr), TeleportRingComponent(nullptr), LeapPawn(nullptr), PlayerCameraManager(nullptr), World(nullptr)
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
-	Root = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
-	RootComponent = Root;
+    Root = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+    RootComponent = Root;
 
-	NSPlayerAreaBounds = Cast<UNiagaraSystem>(StaticLoadObject(UNiagaraSystem::StaticClass(), nullptr,
-		TEXT("NiagaraSystem'/UltraleapTracking/InteractionEngine/VFX/Leap_NS_PlayAreaBounds.Leap_NS_PlayAreaBounds'")));
-	if (NSPlayerAreaBounds == nullptr)
-	{
-		UE_LOG(UltraleapTrackingLog, Error, TEXT("NSPlayerAreaBounds is nullptr in ALeapVisualizer()"));
-	}
+    NSPlayerAreaBounds = Cast<UNiagaraSystem>(StaticLoadObject(UNiagaraSystem::StaticClass(), nullptr,
+                                                               TEXT("NiagaraSystem'/UltraleapTracking/InteractionEngine/VFX/Leap_NS_PlayAreaBounds.Leap_NS_PlayAreaBounds'")));
+    if (NSPlayerAreaBounds == nullptr)
+    {
+        UE_LOG(UltraleapTrackingLog, Error, TEXT("NSPlayerAreaBounds is nullptr in ALeapVisualizer()"));
+    }
 
-	NSPTeleportRing = Cast<UNiagaraSystem>(StaticLoadObject(UNiagaraSystem::StaticClass(), nullptr,
-		TEXT("NiagaraSystem'/UltraleapTracking/InteractionEngine/VFX/Leap_NS_TeleportRing.Leap_NS_TeleportRing'")));
+    NSPTeleportRing = Cast<UNiagaraSystem>(StaticLoadObject(UNiagaraSystem::StaticClass(), nullptr,
+                                                            TEXT("NiagaraSystem'/UltraleapTracking/InteractionEngine/VFX/Leap_NS_TeleportRing.Leap_NS_TeleportRing'")));
 
-	if (NSPTeleportRing == nullptr)
-	{
-		UE_LOG(UltraleapTrackingLog, Error, TEXT("NSPTeleportRing is nullptr in ALeapVisualizer()"));
-	}
+    if (NSPTeleportRing == nullptr)
+    {
+        UE_LOG(UltraleapTrackingLog, Error, TEXT("NSPTeleportRing is nullptr in ALeapVisualizer()"));
+    }
 
-	PlayerAreaBoundsComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("NiagaraAreaBoundsComponent"));
-	TeleportRingComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("NiagaraTeleportRingComponent"));
+    PlayerAreaBoundsComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("NiagaraAreaBoundsComponent"));
+    TeleportRingComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("NiagaraTeleportRingComponent"));
 
-	if (!PlayerAreaBoundsComponent || !TeleportRingComponent)
-	{
-		UE_LOG(UltraleapTrackingLog, Error, TEXT("NSPTeleportRing is nullptr in ALeapVisualizer()"));
-	}
+    if (!PlayerAreaBoundsComponent || !TeleportRingComponent)
+    {
+        UE_LOG(UltraleapTrackingLog, Error, TEXT("NSPTeleportRing is nullptr in ALeapVisualizer()"));
+    }
 
-	if (RootComponent)
-	{
-		PlayerAreaBoundsComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
-		TeleportRingComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
-	}
+    if (RootComponent)
+    {
+        PlayerAreaBoundsComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+        TeleportRingComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+    }
 
-	PlayerAreaBoundsComponent->SetAsset(NSPlayerAreaBounds); 
-	TeleportRingComponent->SetAsset(NSPTeleportRing);
-
+    PlayerAreaBoundsComponent->SetAsset(NSPlayerAreaBounds);
+    TeleportRingComponent->SetAsset(NSPTeleportRing);
 }
 
 // Called when the game starts or when spawned
 void ALeapVisualizer::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	FVector2D PlayerArea = UHeadMountedDisplayFunctionLibrary::GetPlayAreaBounds();
-	FVector PlayerVec = FVector(PlayerArea.X, PlayerArea.Y, 0.0f);
-	PlayerAreaBoundsComponent->SetNiagaraVariableVec3("User.PlayAreaBounds", PlayerVec);
+    FVector2D PlayerArea = UHeadMountedDisplayFunctionLibrary::GetPlayAreaBounds();
+    FVector PlayerVec = FVector(PlayerArea.X, PlayerArea.Y, 0.0f);
+    PlayerAreaBoundsComponent->SetVariableVec3(FName(*FString("User.PlayAreaBounds")), PlayerVec);
 
-	World = GetWorld();
-	if (World == nullptr)
-	{
-		UE_LOG(UltraleapTrackingLog, Error, TEXT("World is nullptr in ALeapVisualizer::BeginPlay"));
-		return;
-	}
+    World = GetWorld();
+    if (World == nullptr)
+    {
+        UE_LOG(UltraleapTrackingLog, Error, TEXT("World is nullptr in ALeapVisualizer::BeginPlay"));
+        return;
+    }
 
-	LeapPawn = UGameplayStatics::GetPlayerPawn(World, 0);
-	if (LeapPawn == nullptr)
-	{
-		UE_LOG(UltraleapTrackingLog, Error, TEXT("LeapPawn is nullptr in ALeapVisualizer::BeginPlay"));
-		return;
-	}
-	PlayerCameraManager = UGameplayStatics::GetPlayerCameraManager(World, 0);
-	if (PlayerCameraManager == nullptr)
-	{
-		UE_LOG(UltraleapTrackingLog, Error, TEXT("PlayerCameraManager is nullptr in ALeapVisualizer::BeginPlay"));
-		return;
-	}
+    LeapPawn = UGameplayStatics::GetPlayerPawn(World, 0);
+    if (LeapPawn == nullptr)
+    {
+        UE_LOG(UltraleapTrackingLog, Error, TEXT("LeapPawn is nullptr in ALeapVisualizer::BeginPlay"));
+        return;
+    }
+    PlayerCameraManager = UGameplayStatics::GetPlayerCameraManager(World, 0);
+    if (PlayerCameraManager == nullptr)
+    {
+        UE_LOG(UltraleapTrackingLog, Error, TEXT("PlayerCameraManager is nullptr in ALeapVisualizer::BeginPlay"));
+        return;
+    }
 }
